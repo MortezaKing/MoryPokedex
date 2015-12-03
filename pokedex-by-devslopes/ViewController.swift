740698b08dbd2d8ce972027462fb0736fa21ed5b
Morteza Kordi <morytala1372@yahoo.com>
2015-12-03 03:54:43 +0330

    Initial Commit

---
+++ pokedex-by-devslopes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-devslopes
+//
+//  Created by Morteza Kordi on 12/3/15.
+//  Copyright © 2015 morytala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
